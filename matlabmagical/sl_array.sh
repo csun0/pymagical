5cd0f8622509a7bf536bda11acf37c84d28fbe0b
csun0 <chris1sun@gmail.com>
2026-03-04 11:23:15 -0500

    test

--- matlabmagical/sl_array.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=512G
-#SBATCH --array=1-1
-
-ARRAY_NUM=$SLURM_ARRAY_TASK_ID
-TOTAL_ARRAY_NUM=$SLURM_ARRAY_TASK_MAX
-
-time matlab -batch "snmulti_main($ARRAY_NUM, 500)"
--- pymagical/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["run_magical"]
-
-def run_magical(*args, **kwargs):
-    from .magical import run_magical as _run_magical
-    return _run_magical(*args, **kwargs)
